d55e2c32d30607b7bc63e51c412a8a8d1928eb39
PanoschiGheorgheCristian <79131710+PanoschiGheorgheCristian@users.noreply.github.com>
2022-10-26 08:27:02 +0300

    Add files via upload

--- Lab4/Lab4.py
+++ Lab4/Lab4.py
@@ -5,7 +5,7 @@ import pymc3 as pm
 import arviz as az
 
 # Dati alfa in minute
-alf = 2
+alf = 4.8
 alf = alf / 60
 
 if __name__ == '__main__':
@@ -14,7 +14,7 @@ if __name__ == '__main__':
         clienti = pm.Poisson('CL', 20)
         plata = pm.TruncatedNormal('P', 0.017, 0.0083, lower=0)
         cook = pm.Exponential('CO', 1/alf)
-        # Serve este timpul necesar ca un client sa fie servit, de cand intra in magazin si pana intra
+        # Serve este timpul necesar ca un client sa fie servit, de cand intra in magazin si pana iese
         serve = pm.Deterministic('S', plata + cook)
         trace = pm.sample(20000)
 
@@ -28,3 +28,7 @@ if __name__ == '__main__':
 
     az.plot_posterior(trace)
     plt.show()
+
+    # Pentru punctul b) am incercat mai multe valori ale lui alfa si am ajuns la concluzia ca alfa = 4,8 este aproximativ valoarea cautata
+    # Valoarea superioara a intervalului HDI pentru acesta e 0.25 ore, adica 15 min (pe graficul lui "serve")
+    # Media lui serve bazata pe alfa este afisata tot pe grafic si e aprox. 0.1 ore, adica 6 min
