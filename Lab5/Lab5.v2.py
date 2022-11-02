261cf3f3d41546511f96a7faecdcb682df33365e
PanoschiGheorgheCristian <79131710+PanoschiGheorgheCristian@users.noreply.github.com>
2022-11-02 20:07:59 +0200

    Add files via upload

--- Lab5/Lab5.v2.py
+++ Lab5/Lab5.v2.py
@@ -2,6 +2,8 @@ from pgmpy.models import BayesianNetwork
 from pgmpy.factors.discrete import TabularCPD
 from pgmpy.inference import VariableElimination
 
+# Aici am modificat valorile din Lab5.py pentru un rezultat diferit (din punct de vedere numeric)
+
 model = BayesianNetwork([("Cartej1", "Cartej2"), 
                          ("Cartej1", "Deciziej1"), 
                          ("Deciziej1", "Deciziej2"), 
@@ -28,19 +30,19 @@ cpd_cartej2 = TabularCPD("Cartej2", 5, [ [0, 0.25, 0.25, 0.25, 0.25],
 
 # Decizie = 0 <=> jucatorul a asteptat    
 
-cpd_deciziej1 = TabularCPD("Deciziej1", 2, [ [0, 0.25, 0.5, 0.75, 1], 
-                                            [1, 0.75, 0.5, 0.25, 0] ], 
+cpd_deciziej1 = TabularCPD("Deciziej1", 2, [ [0, 0.15, 0.5, 0.85, 1], 
+                                            [1, 0.85, 0.5, 0.15, 0] ], 
                            evidence=["Cartej1"],
                            evidence_card=[5]
                            )
-cpd_deciziej2 = TabularCPD("Deciziej2", 2, [ [0, 0, 0, 0.33, 0.33, 0.66, 0.66, 1, 1, 1],
-                                            [1, 1, 1, 0.66, 0.66, 0.33, 0.33, 0, 0, 0] 
+cpd_deciziej2 = TabularCPD("Deciziej2", 2, [ [0, 0, 0, 0.20, 0.20, 0.80, 0.80, 1, 1, 1],
+                                            [1, 1, 1, 0.80, 0.80, 0.20, 0.20, 0, 0, 0] 
                                             ],
                            evidence=["Cartej2", "Deciziej1"],
                            evidence_card=[5,2]
                            )
-cpd_deciziej1_prim = TabularCPD("Deciziej1_prim", 2, [ [0, 0, 0, 0.33, 0, 0.66, 0.5, 1, 1, 1],
-                                                      [1, 1, 1, 0.66, 1, 0.33, 0.5, 0, 0, 0] ],
+cpd_deciziej1_prim = TabularCPD("Deciziej1_prim", 2, [ [0, 0, 0, 0.20, 0, 0.80, 0.5, 1, 1, 1],
+                                                      [1, 1, 1, 0.80, 1, 0.20, 0.5, 0, 0, 0] ],
                                 evidence=["Cartej1", "Deciziej2"],
                                 evidence_card=[5,2]
                                 )
